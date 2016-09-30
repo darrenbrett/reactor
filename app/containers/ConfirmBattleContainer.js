fb09025438f2331da4a5108feaf6e3d18d32e5b1
Darren King <darrenbrettking@hotmail.com>
2016-09-30 11:50:14 -0700

    Added battle setup

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function() {
+        return {
+            isLoading: true,
+            playerInfo: []
+        }
+    },
+    componentDidMount() {
+        var query = this.props.location.query;
+        // Fetch gethub info, update state
+    },
+    render: function() {
+        return (
+            <ConfirmBattle isLoading={this.state.isLoading}
+            playerInfo={this.state.playerInfo} />
+        );
+    }
+});
+
+module.exports = ConfirmBattleContainer
