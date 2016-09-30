898cd0083ec21cd693389b3715119c605fe7989b
Darren King <darrenbrettking@hotmail.com>
2016-09-30 10:09:41 -0700

    Inital commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+ReactDOM.render(routes, document.getElementById('app'));
